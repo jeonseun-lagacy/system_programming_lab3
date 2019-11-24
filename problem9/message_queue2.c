2cd748555f79dede1d1784fad2844b5a9cf858a9
전세운 <wjstpdns2015@naver.com>
2019-11-24 13:18:54 +0900

    시스템 프로그래밍 lab3 (문제9번)
    메시지를 전송하는 프로세스와 받는 프로세스로 구분

--- problem9/message_queue2.c
+++ problem9/message_queue2.c
@@ -6,40 +6,27 @@
 #include <string.h>
 
 #define BUFFER 256
-#define QNAME "/msg_queue"
-#define PRIORITY 1
 
 int main(){
     char *msg[BUFFER];
-    int prio;
     mqd_t msg_queue;
     struct mq_attr q_attr;
     q_attr.mq_maxmsg = 10;
     q_attr.mq_msgsize = BUFFER;
 
-    if((msg_queue = mq_open(QNAME, O_CREAT | O_RDWR | O_NONBLOCK, 0600, &q_attr)) == -1){
+    if((msg_queue = mq_open("/msg_queue", O_CREAT | O_RDWR, 0666, &q_attr)) == -1){
         perror("mq_open");
         exit(1);
     }
     
     while(1){
-        printf("send msg :");
-        gets(msg);
-        if(mq_send(msg_queue, msg, strlen(msg), PRIORITY)==-1){
-            perror("mq_send");
+        if(mq_receive(msg_queue, msg, BUFFER, NULL)== -1){
+            perror("mq_receive");
             exit(1);
         }
+        printf("receive : %s\n", msg);
         if(!strcmp(msg, "end")){break;}
         memset(msg, 0, 256);
     }
-    if (mq_close(msg_queue) == -1) {
-        perror ("mq_close");
-        exit (1);
-    }
-    if (mq_unlink(QNAME) == -1){
-        perror("mq_unlink");
-        exit(1);
-    }
-
     return 0;
 }
